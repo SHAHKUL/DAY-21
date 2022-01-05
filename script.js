a93b93ffeed0dca7841f5edd5a59d763fe4af2a1
SHAHKUL <94035362+SHAHKUL@users.noreply.github.com>
2022-01-05 11:57:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+function foo(){
+const getdata=()=>{
+setTimeout(()=>{
+
+var demo1=document.getElementById('demo10').innerHTML=10
+setTimeout(()=>{
+    var demo2=document.getElementById('demo9').innerHTML=9
+    setTimeout(()=>{
+        var demo2=document.getElementById('demo8').innerHTML=8
+        setTimeout(()=>{
+            var demo2=document.getElementById('demo7').innerHTML=7
+            setTimeout(()=>{
+                var demo2=document.getElementById('demo6').innerHTML=6
+                setTimeout(()=>{
+                    var demo2=document.getElementById('demo5').innerHTML=5
+                    setTimeout(()=>{
+                        var demo2=document.getElementById('demo4').innerHTML=4
+                        setTimeout(()=>{
+                            var demo2=document.getElementById('demo3').innerHTML=3
+                            setTimeout(()=>{
+                                var demo2=document.getElementById('demo2').innerHTML=2
+                                setTimeout(()=>{
+                                    var demo2=document.getElementById('demo1').innerHTML=1
+                                    setTimeout(()=>{
+                                        var demo2=document.getElementById('demo').innerHTML="HAPPY INDEPENDENCE DAY!!!"
+                                       
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+},1000)
+}
+getdata()
+}
